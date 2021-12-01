576c6617b2c0f3b5cb0cc1c6069e058f04e30838
kajalusadadiya <kajalvpatel07@gmail.com>
2021-12-01 00:02:10 +0000

    This is my homework-13 nopcomm

---
+++ src/main/java/com/nopcommerce/demo/pages/HomePage.java
@@ -0,0 +1,28 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utility.Utility;
+
+public class HomePage extends Utility {
+ public void clickOnGiftCardsLink() {
+ }
+
+ public void clickOnComputerLink() {
+ }
+
+ public void clickOnDigitalDownloadsLink() {
+ }
+
+ public void clickOnJewleryLink() {
+ }
+
+ public void clickOnElectronicsLink() {
+ }
+
+ public void clickOnBooksLink() {
+ }
+
+ public void clickOnApparelLink() {
+ }
+
+
+}
